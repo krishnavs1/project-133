c3fd21387743e10bf187a4a7404e2db1041f75a4
krishnavs1 <75359090+krishnavs1@users.noreply.github.com>
2021-09-10 13:55:19 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+img1="";
+img2="";
+img3="";
+img4="";
+img5="";
+img6="";
+status="";
+objects=[];
+function preload(){
+img1="pillow.HEIC";
+img4="tv.HEIC";
+img6="rubixcube.HEIC";
+img5="bottle.HEIC";
+img3="tissuebox.HEIC";
+img2="book.jpg";
+  }
+  
+  
+  
+
+
+
+function loadimg1() {
+    window.location.href="pillow.html";
+}
+function loadimg2() {
+  window.location.href="book.html";
+}
+function loadimg3() {
+  window.location.href="tissuebox.html";
+}
+function loadimg4() {
+  window.location.href="tv.html";
+}
+function loadimg5() {
+  window.location.href="bottle.html";
+}
+function loadimg6() {
+  window.location.href="rubixcube.html";
+}
